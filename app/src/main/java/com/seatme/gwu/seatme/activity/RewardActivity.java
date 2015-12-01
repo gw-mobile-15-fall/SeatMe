23dabde53b177e91802ed6bd09f21c354d4f5860
thebirdyan <thebirdyan@gmail.com>
2015-11-30 21:21:16 -0500

    Add reward and profile activity

---
+++ app/src/main/java/com/seatme/gwu/seatme/activity/RewardActivity.java
@@ -0,0 +1,15 @@
+package com.seatme.gwu.seatme.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.seatme.gwu.seatme.R;
+
+public class RewardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reward);
+    }
+}
